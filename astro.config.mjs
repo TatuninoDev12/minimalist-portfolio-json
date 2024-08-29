815d215aa94f1d311155ba1c79cb0cb627e1c3b0
Emir Tactuk Paulino <33699141+TatuninoDev12@users.noreply.github.com>
2024-08-29 03:03:17 -0400

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next()],
+  // output: "static",
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true,
+  //   },
+  // }),
 });
